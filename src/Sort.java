1a3532ba32ae735d689b6aa99f973d36b2df79c9
Md Jahidul Islam <mdjahidulislamfuad@gmail.com>
2025-09-20 00:24:05 +0600

    Bubble sort of an array

---
+++ src/Sort.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Sort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.print("Sorted array: ");
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+
+        sc.close();
+    }
+}
