e178ffd8b1d9207edb5e7eb1fa58aeb76d201f3a
Md Jahidul Islam <mdjahidulislamfuad@gmail.com>
2025-09-19 23:10:24 +0600

    Largest number of array

---
+++ src/LargestNumber.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LargestNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("The largest number is: " + max);
+
+        sc.close();
+    }
+}
