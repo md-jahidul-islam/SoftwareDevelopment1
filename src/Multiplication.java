210e22e07d218cd78c4111921ab8df13960ca86f
Md Jahidul Islam <mdjahidulislamfuad@gmail.com>
2025-09-19 22:47:37 +0600

    multiplication table

---
+++ src/Multiplication.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Multiplication {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.println("\nMultiplication Table for " + n + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " X " + i + " = " + (n * i));
+        }
+
+        sc.close();
+    }
+}
